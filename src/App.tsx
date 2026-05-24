a4c4b435b945a908342ec650788b2b81d76d1468
TheLis3k <ksawery2k19@gmail.com>
2026-05-24 13:07:59 +0200

    feat: add Loader component for improved loading experience

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Loader from './components/Loader'
 import ParticlesCanvas from './components/ParticlesCanvas'
 import HeroSection from './components/HeroSection'
 import AboutMe from './components/AboutMe'
@@ -5,6 +6,7 @@ import AboutMe from './components/AboutMe'
 export default function App() {
   return (
     <div className="antialiased relative bg-lisek-dark">
+      <Loader />
       <ParticlesCanvas />
       <HeroSection />
       <AboutMe />
